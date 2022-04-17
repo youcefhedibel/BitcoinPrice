dc297213d1e45686cf22081d0ea71453985f87fa
YoucefHedibel <34919049+youcefhedibel@users.noreply.github.com>
2022-04-17 09:37:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const url = "https://blockchain.info/ticker";
+
+function recupererPrix(){
+  
+
+
+//créer une requette
+let request = new XMLHttpRequest(); // créer un objet
+request.open('GET',url); // premier parametre GET / POST deuxieme parametre URL
+request.responseType ='json'; //nous attendons de JSON
+request.send(); //nous envoyons notre requette
+
+//dé qu'on reçoit une réponse cette fonction est exécuté
+
+request.onload = function(){
+  if(request.readyState === XMLHttpRequest.DONE) {
+    
+    if(request.status === 200){
+      
+      let response = (parseFloat(request.response.EUR.last.toFixed(2))*200).toFixed(2);
+      //console.log(response);
+      document.querySelector('#price_label').textContent = response;
+      
+    } else {
+      alert("un probleme est intervenue , veillez reseillez plus tard..")
+    }
+    
+  }
+}
+  }
+  
+  //refrsh the price after 5 seconds
+setInterval(recupererPrix, 5000);
